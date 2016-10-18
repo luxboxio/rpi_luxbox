f31c335692fae899bad47f6d31bb07d717ccb882
noppelmax <noppelmax@googlemail.com>
2016-10-18 18:50:52 +0000

    implement fade algo

--- luxbox_receiver.py
+++ luxbox_receiver.py
@@ -4,10 +4,14 @@ import socket
 import sys
 import ws2811
 import json
+import time
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 11111
 RGBFILE = "/etc/luxbox/rgb.data"
+FADETIME = 2
+FADEFRAMERATE = 30
+FADETOTALFRAMES = FADEFRAMERATE * FADETIME
 
 print("Listening at ip: ", UDP_IP, ":", UDP_PORT)
 
@@ -21,17 +25,17 @@ try:
   f = open(RGBFILE, 'r')
   rgbdata = f.read().split(',')
   try:
-    r = rgbdata[0]
+    r = int(rgbdata[0])
   except IndexError:
     r = 0
     
   try:
-    g = rgbdata[1]
+    g = int(rgbdata[1])
   except IndexError:
     g = 0
 
   try:
-    b = rgbdata[2]
+    b = int(rgbdata[2])
   except IndexError:
     b = 0
 
@@ -45,19 +49,41 @@ except IOError:
 
 while True:
   data, addr = sock.recvfrom(4096)
-  print("received message: ", data)
+  #print("received message: ", data)
   jsondata = json.loads( str(data, "utf-8") );
   for area in jsondata['areas']:
-    print(area)
+    #print(area)
     for value in area['values']:
-      print(value)
+      #print(value)
       if value['color'] == 'r':
-        r = value['value']
+        targetRed = value['value']
       elif value['color'] == 'g':
-        g = value['value']
+        targetGreen = value['value']
       elif value['color'] == 'b':
-        b = value['value']
-    ws2811.render( r, g, b)
+        targetBlue = value['value']
+      else:
+        print("Received unknown color")
+
+    stepRed = (targetRed - r) / FADETOTALFRAMES
+    stepGreen = (targetGreen - g) / FADETOTALFRAMES
+    stepBlue = (targetBlue - b) / FADETOTALFRAMES
+    #print("Fading to "+str(targetRed)+","+str(targetGreen)+","+str(targetBlue))
+    #print("Fading from "+str(r)+","+str(g)+","+str(b))
+    #print("Steps to "+str(stepRed)+","+str(stepGreen)+","+str(stepBlue))
+    for frame in range(0,FADETOTALFRAMES):
+      newRed = int( r + (frame * stepRed))
+      newGreen = int( g + (frame * stepGreen))
+      newBlue = int( b + (frame * stepBlue))
+      #print("New is "+str(newRed)+","+str(newGreen)+","+str(newBlue))
+      ws2811.render( newRed, newGreen, newBlue )
+      time.sleep(FADETIME / FADETOTALFRAMES)
+
+    # Reached target color
+    r = targetRed
+    g = targetGreen
+    b = targetBlue
+    ws2811.render( r, g, b )
+    
     try:
       f = open(RGBFILE, 'w')
       f.write( str(r) + ",")
